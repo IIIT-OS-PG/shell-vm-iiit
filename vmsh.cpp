694e943ff2f1f864a8ef3af90c82b4a93e9a3dfb
vm-iiit <virat.mishra@students.iiit.ac.in>
2019-09-03 18:21:31 +0530

    version 2

--- vmsh.cpp
+++ vmsh.cpp
@@ -1,34 +1,78 @@
 #include<iostream>
 #include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 using namespace std;
-#define max_word_len = 20;
-#define max_words = 50;
+#define max_word_len 20
+#define max_words 50
 
 int main()
 {
-    char *charr[10];
+    //char *charr[10];
     char ch;
     int lv = -1;
     while(1)
     {
+        cout<<getenv("USER")<<'@';
+        //cout<<getenv("HOSTNAME");
+        cout<<':'<<getenv("PWD")<<": ";
+
+        char **charr = new char*[max_words];
     	do{
     	    ++lv;
-    	    charr[lv] = new char[10];
+    	    charr[lv] = new char[max_word_len];
     	    cin>>charr[lv];
-    	    cout<<lv<<" took inp of "<<charr[lv]<<endl;
+    	    //cout<<lv<<" took inp of "<<charr[lv]<<endl;
     	    ch = getchar();
     	}while(ch != '\n');
     	++lv;
     	charr[lv] = NULL;
-    	cout<<"loop done\n";
-    	while(lv>-1)
+        //cout<<"     lv end "<<lv<<endl;
+    	//cout<<"loop done\n";
+    	/*while(lv>-1)
     	{
         	cout<<lv<<endl;
         	if(charr[lv])
             	cout<<charr[lv]<<endl;
         	else
-            	cout<<"nulla\n";
+            	cout<<"null\n";
             --lv;
-    	}
+    	}*/
+        if(strcmp(charr[0], "exit") == 0)
+        {
+            break;
+            //delete charr;
+        }
+        int f = fork();
+        if(f)
+        {
+            //cout<<"     inside parent\n";
+            wait(NULL);
+            //cout<<"     parent's wait ended\n";
+        }
+        else
+        {
+            //cout<<"     inside child\n";
+            int s_f = execvp(charr[0], charr);
+            if(s_f == -1)
+                cout<<"     error\n";
+            //cout<<"     exiting child\n";
+            exit(0);
+        }
+        //cout<<f<<"      deleting memory\n";
+        --lv;
+        //cout<<"lv "<<lv<<endl;
+        while(lv>-1)
+        {
+            //cout<<f<<"      deleting "<<charr[lv]<<endl;
+            delete charr[lv];
+            //cout<<f<<"      deleted\n";
+            --lv;
+        }
+        //cout<<f<<"      deleted indivs\n";
+        delete charr;
     }
+    
+    return 0;
 }
