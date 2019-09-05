dd6a74b981e9821275686c578a36de26d4c872bc
vm-iiit <virat.mishra@students.iiit.ac.in>
2019-09-06 01:23:41 +0530

    io redirection >

--- vmsh.cpp
+++ vmsh.cpp
@@ -59,6 +59,20 @@ void prompt()
     carrptr[lv+1] = NULL;
 }*/ 
 
+void eval_pipe(char** ptr)
+{
+    int lv, count=0, lc=0;
+    int start, end;
+    for(lv=0; ptr[lv] != NULL; lv++)
+    {
+        start = lv;
+        end = lv+1;
+        while(strcmp(ptr[end], "|") != 0 && ptr[end] != NULL)
+            ++end;
+        
+    }
+}
+
 void child_handler(char** parr, bool pflag, bool rtflag)
 {
     ll lv;
@@ -66,27 +80,35 @@ void child_handler(char** parr, bool pflag, bool rtflag)
     bool decide;
     char pathh[30];
     getcwd(pathh, 30);
+    for(lv=0; parr[lv] != NULL; lv++);
     if(pflag || rtflag)
     {
         if(!pflag && rtflag)
         {
-            for(lv=0; parr[lv] != NULL; lv++);
-            cout<<"runnin\n";
+            
+            //cout<<"runnin\n";
             lv -= 2;
-            cout<<parr[lv+1]<<endl;
+            //cout<<parr[lv+1]<<endl;
             //strcat(pathh, parr[lv+1]);
             //cout<<pathh<<endl;
             
             decide = (strcmp(parr[lv], ">") == 0)?true:false;
-            fd = open(parr[lv+1], O_APPEND | O_CREAT | 0640);
+            if(!decide)
+                fd = open(parr[lv+1], O_RDWR | O_CREAT | O_APPEND);
+            else
+                fd = open(parr[lv+1], O_WRONLY | O_CREAT | O_TRUNC );
             parr[lv] = NULL;
+            chmod(parr[lv+1], S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
             cout<<"fd "<<fd<<endl;
-            close(1);
+            //close(1);
             dup2(fd, 1);
-            
             execvp(parr[0], parr);
             close(fd);
-            cout<<"done dana done dan\n";
+            //cout<<"done dana done dan\n";
+        }
+        else if(pflag && !rtflag)
+        {
+            eval_pipe(parr);    
         }
 
     }
@@ -130,8 +152,8 @@ int main()
     	++lv;
     	charr[lv] = NULL;
         int lv = 0;
-        while(charr[lv])
-            cout<<charr[lv++]<<endl;
+        //while(charr[lv])
+          //  cout<<charr[lv++]<<endl;
         //cout<<"     lv end "<<lv<<endl;
     	//cout<<"loop done\n";
     	/*while(lv>-1)
